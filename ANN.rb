86e91ef419ccb7d3cee27ebc298cfe334f23d117
Marshal Ma <mashuo93@gmail.com>
2016-03-23 14:22:08 -0500

    all module tested and worked, next step is to add command flags

--- ANN.rb
+++ ANN.rb
@@ -1,23 +1,32 @@
 require 'matrix'
 
-def ann_learn(examples, target, units, epoch = 10000, learning_rate=0.1)
+def ann_learn(examples, target, examples_val, target_val, units, epoch_max = 2000, learning_rate = 0.1, momentum_rate = 0.05)
   num_input_unit = units.first
   num_output_unit = units.last
   num_of_layers = units.size
   num_of_hidden_layers = num_of_layers - 2
 
-  max_arr, min_arr = normalize!(examples)
-  mapping = encode_target!(target)
+  mapping = encode_target!(target, units.last)
 
   # initialize weight vectors randomly.
   weights = []
+  last_delta_weights = [] # for the purpose of adding momentum to back prop algorithm
   (0..num_of_layers-2).each { |i| weights << ( Matrix.build(units[i]+1, units[i+1]) {rand} ).to_a }
+  (0..num_of_layers-2).each { |i| last_delta_weights << ( Matrix.build(units[i]+1, units[i+1]) {0} ).to_a }
 
   # implementation of Back Propagation Algorithm
   # main loop
-  (0..epoch-1).each do |iter|
+  max_accuracy = 0
+  max_accuracy_iter = 0
+  max_accuracy_weights = nil
+  (0..epoch_max-1).each do |iter|
+    # stop criteria
+    break if 2 * max_accuracy_iter < iter && iter.to_f > 0.1 * epoch_max.to_f
+
+    # puts weights.to_s
     examples.each_with_index do |example, index|
-      activation = calc_activation(example, weights)
+      # puts index.to_s
+      activation = activation_calc(example, weights)
       deltas = []
 
       # output unit delta
@@ -25,51 +34,90 @@ def ann_learn(examples, target, units, epoch = 10000, learning_rate=0.1)
 
       # hidden unit delta
       (num_of_hidden_layers-1).downto(0) do |i|
-        deltas << hidden_delta_cal(activation[i], weight[i+1], deltas.last)
+        deltas << hidden_delta_cal(activation[i], weights[i+1], deltas.last)
       end
 
       # reverse deltas
       deltas.reverse!
 
       # update weights
-      update_weights(weights, deltas, [examples] + activations, learning_rate)
-     end
+      last_delta_weights = update_weights(weights, last_delta_weights, deltas, [example] + activation, learning_rate, momentum_rate)
+    end
+
+    # track TRAINING AND VALIDATION MSE
+    raw_predictions, predictions = ann_predict(examples, weights, mapping)
+    raw_predictions_val, predictions_val = ann_predict(examples_val, weights, mapping)
+    accuracy_val = ann_evaluate(examples_val, target_val, weights, mapping)
+
+    # maintain the weights of the epoch that has the maximum accuracy on validation set
+    if accuracy_val > max_accuracy
+      max_accuracy = accuracy_val
+      max_accuracy_iter = iter
+      max_accuracy_weights = weights
+    end
+
+    # print out result of this epoch
+    puts "--------------------------------------------------------------------------------------"
+    puts "epoch =  #{iter.to_s}, TRAINING MSE = #{mse_calc raw_predictions, target}, VALIDATION MSE = #{mse_calc raw_predictions_val, target_val}"
+    puts "current accuracy = #{accuracy_val}, highest accuracy epoch = #{max_accuracy_iter}, highest accuracy = #{max_accuracy}"
+
   end
-  [weights, max_arr, min_arr, mapping]
+  [max_accuracy_weights, mapping]
 end
 
-def ann_evaluate(examples, target, weights, max_arr, min_arr, mapping)
-  predictions = ann_predict(examples, weights, max_arr, min_arr, mapping)
+def ann_evaluate(examples, target, weights, mapping)
+  raw_prediction, predictions = ann_predict(examples, weights, mapping)
+
   num_of_accurate_predictions = 0
   predictions.each_with_index {|val,i| num_of_accurate_predictions += 1 if val == target[i]}
   return num_of_accurate_predictions.to_f / predictions.size.to_f
 end
 
-def ann_predict(examples, weights, max_arr, min_arr, mapping)
-  normalize_with_range! examples, max_arr, min_arr
-  predictions = []
+def ann_predict(examples, weights, mapping)
+  raw = []
+
   examples.each do |input|
-    activation = activation_calc input weights
-    predictions << activation.last
+    activation = activation_calc input, weights
+    raw << activation.last
   end
+
+  predictions = Marshal.load(Marshal.dump(raw))
+  predictions.each do |prediction|
+    max = prediction[0]
+    index = 0
+    prediction.size.times do |i|
+      if max < prediction[i]
+        max = prediction[i]
+        index = i
+      end
+    end
+    prediction.size.times do |i|
+      prediction[i] = 0 if index != i
+      prediction[i] = 1 if index == i
+    end
+  end
+
   decode_target! predictions, mapping
-  predictions
+  # binding.pry
+  [raw, predictions]
 end
 
 def activation_calc(input, weights)
   result = []
   weights.each do |weight|
-    input = sigmoid( (Matrix.row_vector([1] + input) * Matrix.rows(weight)).to_a )
+    input = sigmoid( (Matrix.row_vector([1] + input) * Matrix.rows(weight)).to_a.first )
     result << input
   end
   result
 end
 
-  def sigmoid(vector)
-    result = []
-    vector.each {|item| result << 1/(1 + Math.exp(-item))}
-    result
-  end
+    def sigmoid(vector)
+      result = []
+      vector.each do |item|
+        result << 1/(1 + Math.exp(-item))
+      end
+      result
+    end
 
 def output_delta_calc(o, t)
   result = []
@@ -87,14 +135,23 @@ def hidden_delta_cal(o, weight, delta)
   result
 end
 
-def update_weights(weights, deltas, activations, rate)
+def update_weights(weights, last_deltas_weights, deltas, activations, learning_rate, momentum_rate)
+  # puts deltas.to_s
+  # puts activations.to_s
+  delta_weights = Marshal.load(Marshal.dump(last_deltas_weights))
   weights.each_with_index do |weight, iter|
-    weight.each_with_index do |row, j|
-      row.each_with_index do |val, i|
-        weights[iter][i][j] = rate * deltas[iter][j] * activations[iter]
+    weight.each_with_index do |row, i|
+      row.each_with_index do |val, j|
+        if i == 0
+          delta_weights[iter][i][j] = learning_rate * deltas[iter][j] + momentum_rate * last_deltas_weights[iter][i][j]
+        else
+          delta_weights[iter][i][j] = learning_rate * deltas[iter][j] * activations[iter][i-1] + momentum_rate * last_deltas_weights[iter][i][j]
+        end
+        weights[iter][i][j] += delta_weights[iter][i][j]
       end
     end
   end
+  delta_weights
 end
 
 def encode_target!(target, num_of_output_units)
@@ -114,38 +171,19 @@ def decode_target!(target, mapping)
   end
 end
 
-def normalize!(examples)
-  num_of_attr = examples[0].size
-  max_arr = []; min_arr = []
-  (0..num_of_attr-1).each do |i|
-    min = examples[0][i]
-    max = examples[0][i]
-    examples.each do |item|
-      min = item[i] if item[i] < min
-      max = item[i] if item[i] > max
-    end
-    max_arr << max.to_f; min_arr << min.to_f
-    examples.each_with_index do |item,index|
-      if min == max
-        examples[index][i] = 0.to_f; next
-      end
-      examples[index][i] = (examples[index][i].to_f - min.to_f) / (max.to_f - min.to_f)
-    end
+def mse_calc(predictions, targets)
+  result = 0
+  predictions.size.times do |i|
+    subresult = 0
+    predictions[i].size.times {|j| subresult += (predictions[i][j] - targets[i][j]) ** 2}
+    result += 0.5 * subresult
   end
-  [max_arr, min_arr]
+  result /= predictions.size.to_f
 end
 
-def normalize_with_range!(examples, max_arr, min_arr)
-  num_of_attr = examples[0].size
-  (0..num_of_attr-1).each do |i|
-    examples.each_with_index {|v,index| examples[index][i] = (examples[index][i].to_f - min_arr[i].to_f) / (max_arr[i].to_f - min_arr[i].to_f)}
-  end
-end
-
-
+# SIMPLE TESTS (be advised that some of the methods are obsolete)
 # puts encode_target([3],5).to_s # test of encode_target
 # puts (decode_target!([[0,0,0,1,0,0]], encode_target!([3,4,5],6)) ).to_s
-
 # examples = [[5.7, 3.8, 1.7, 0.3, 0], [6.5, 3.0, 5.2, 2.0, 2], [6.9, 3.2, 5.7, 2.3, 2], [5.1, 3.4, 1.5, 0.2, 0], [6.3, 2.8, 5.1, 1.5, 2], [7.7, 2.6, 6.9, 2.3, 2], [5.7, 3.0, 4.2, 1.2, 1], [5.4, 3.4, 1.7, 0.2, 0], [7.3, 2.9, 6.3, 1.8, 2], [6.2, 2.9, 4.3, 1.3, 1], [5.8, 2.7, 5.1, 1.9, 2], [4.9, 2.5, 4.5, 1.7, 2], [5.1, 3.5, 1.4, 0.3, 0], [6.3, 2.5, 5.0, 1.9, 2], [4.3, 3.0, 1.1, 0.1, 0], [5.0, 3.0, 1.6, 0.2, 0], [6.7, 2.5, 5.8, 1.8, 2], [4.4, 3.0, 1.3, 0.2, 0], [5.0, 3.2, 1.2, 0.2, 0], [6.7, 3.0, 5.2, 2.3, 2], [4.4, 2.9, 1.4, 0.2, 0], [5.1, 3.8, 1.9, 0.4, 0], [5.4, 3.9, 1.3, 0.4, 0], [5.7, 2.6, 3.5, 1.0, 1], [7.2, 3.6, 6.1, 2.5, 2], [5.0, 2.0, 3.5, 1.0, 1], [5.5, 4.2, 1.4, 0.2, 0], [5.8, 2.8, 5.1, 2.4, 2], [6.7, 3.0, 5.0, 1.7, 1], [6.7, 3.3, 5.7, 2.5, 2], [7.9, 3.8, 6.4, 2.0, 2]]
 # examples.each {|item| puts item.to_s}
 # mapping = normalize!(examples)
@@ -154,6 +192,5 @@ end
 # normalize_with_range! examples1, mapping[0], mapping[1]
 # examples1.each {|item| puts item.to_s}
 # puts examples.to_s
-
 # puts sigmoid([0,1,-1,1000,-1000]).to_s
 # ann_learn([[1,2],[1,3],[2,3],[2,2],[1,1],[3,3],[3,1],[3,2],[2,1],[1,0],[3,0]], nil, [2,3,1])
--- Preprocess.rb
+++ Preprocess.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Preprocess
   def initialize(file_path)
     @data = CSV.read(file_path)
@@ -9,11 +11,11 @@ class Preprocess
     create_discrete_mapping()
     encode_discrete_var()
     shuffle()
+    normalize!(@data, [@number_of_features-1])
   end
 
   def find_discrete_continuous_variables
     # :d_var, :c_var represents discrete and continuous variable
-
     @var_type = [];
     @data[0].each do |var|
       @var_type << (nan?(var) ? :d_var : :c_var)
@@ -48,7 +50,7 @@ class Preprocess
   def encode_discrete_var
     @var_type.each_with_index do |item,i|
       next if item != :d_var
-      (0..@data.size).each do |index|
+      @data.size.times do |index|
         @data[index][i] = @mapping[i][@data[index][i]]
       end
     end
@@ -74,6 +76,62 @@ class Preprocess
       all_values[item[attr_index]] = true if !all_values.has_key? item
     end
     return all_values.keys
+  end
+
+  def normalize!(examples, without_index)
+    num_of_attr = examples[0].size
+    max_arr = []; min_arr = []
+    (0..num_of_attr-1).each do |i|
+      next if without_index.include? i
+
+      min = examples[0][i]
+      max = examples[0][i]
+      examples.each do |item|
+        min = item[i] if item[i] < min
+        max = item[i] if item[i] > max
+      end
+      max_arr << max.to_f; min_arr << min.to_f
+      examples.each_with_index do |item,index|
+        if min == max
+          examples[index][i] = 0.to_f; next
+        end
+        examples[index][i] = (examples[index][i].to_f - min.to_f) / (max.to_f - min.to_f)
+      end
+    end
+    [max_arr, min_arr]
+  end
+
+  def self.separate_attr_and_target(data)
+    attribute = []
+    target = []
+    (0..data.size-1).each do |i|
+      attribute << data[i][0..data[i].size-2]
+      target << data[i].last
+    end
+    [attribute, target]
+  end
+
+  def self.divide_dataset(data, i, num_of_fold)
+    num_in_one_fold = data.size / 10
+
+    training_set = []
+    validation_set = []
+    test_set = []
+
+    range = (i*num_in_one_fold..(i+1)*num_in_one_fold-1)
+    data.size.times do |j|
+      if range.include? j
+        test_set << data[j]
+      else
+        training_set << data[j]
+      end
+    end
+
+    split = training_set.size * 2 / 3
+    validation_set = training_set[split..training_set.size-1]
+    training_set = training_set[0..split-1]
+
+    [training_set, validation_set, test_set]
   end
 
 end
